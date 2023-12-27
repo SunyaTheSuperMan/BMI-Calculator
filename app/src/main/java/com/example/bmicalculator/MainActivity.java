32db98d761086ce760f8687caf6eee17ede14330
SunyaTheSuperMan <nanhesunil098@gmail.com>
2023-12-27 13:46:07 +0530

    BMI Project

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
-
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.graphics.Color;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextHeight = findViewById(R.id.height);
         Button button = findViewById(R.id.btnSubmit);
         TextView textView = findViewById(R.id.result);
+        CardView card = findViewById(R.id.status);
 
         button.setOnClickListener (new View.OnClickListener(){
             @Override
@@ -29,22 +30,18 @@ public class MainActivity extends AppCompatActivity {
                 float bmi = weight / (height * height);
                 textView.setText(String.valueOf(bmi));
 
-
                 if (bmi > 24) {
                     textView.setText("Over Weight: " + bmi);
-
+                    card.setCardBackgroundColor(Color.parseColor("#FFA500"));
                 } else if (bmi>18) {
                     textView.setText("Normal Weigt :"+bmi);
-
+                    card.setCardBackgroundColor(Color.parseColor("#19B81F"));
                 }
                 else {
                     textView.setText("Under Weight :"+bmi);
+                    card.setCardBackgroundColor(Color.parseColor("#ff0000"));
                 }
-
-
             }
         });
-
-
     }
 }
